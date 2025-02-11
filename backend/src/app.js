2b9a232f0e9e15acb88c385d56100490f4848c95
chance-steven <michelhategekimana@127gmail.com>
2025-02-11 11:15:46 +0200

    chore:setup backend

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`server started at http://localhost:${PORT}`);
+});
